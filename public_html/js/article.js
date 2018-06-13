ac3781d9607b93d6c2071dee66c22c5dd3bea639
BadarouxY <badaroux.yannick@gmail.com>
2018-06-13 15:28:58 +0200

    commit yann

--- public_html/js/article.js
+++ public_html/js/article.js
@@ -20,14 +20,37 @@ function loadArticle()
     var url = window.location.search;
     var id = url.substring(url.lastIndexOf("=") + 1);
 
-     $.getJSON("http://172.16.153.31:8080/sebo_backendnew/api/productscatalogue/getoneproduct/" + id, function (article) {
+    $.getJSON("http://172.16.153.31:8080/sebo_backendnew/api/productscatalogue/getoneproduct/" + id, function (article) {
 
         $("#artTitre").append(article.name);
         $("#artEditeur").append(article.supplier);
         $("#artRef").append(article.reference);
         $("#artDescription").append('super produit');
-        $("#artPrice").append(article.price + '€');    
+        $("#artPrice").append(article.price + '€');
     }
     );
 
+}
+
+function loadArticlesByCategory()
+{
+    var url = window.location.search;
+    var category = url.substring(url.lastIndexOf("=") + 1);
+    $.getJSON("http://172.16.153.31:8080/sebo_backendnew/api/productscatalogue/getlistbycategory/" + category, function (articles) {
+
+        $.each(articles, function (i, value) {
+            $("#catalogue").append('<div class="articleVignette" id="'+ i + '"></div>');
+            
+            
+            $("#" + i).append('<h3>' + value.name + '</h3>');
+            $("#" + i).append('<hr>');
+            $("#" + i).append('<img src="' + value.urlPicture + '"height="70" width="70" />');
+            $("#" + i).append('<p>' + value.price + '€</p>');
+            $("#" + i).append('<a href="article.html?id=' + value.id + '"><button class="btn">Voir l\'article</button></a>');
+    
+        }
+
+        );
+    }
+    );
 }
