a5c36411f557cd5c5744e0ee8cca1e55804e896b
hako <DJENADI@dev.afpa>
2018-06-13 16:30:29 +0200

    MAJ page produit + prix dynamique

--- public_html/js/article.js
+++ public_html/js/article.js
@@ -1,31 +1,18 @@
-<<<<<<< HEAD
-function loadArticle()
-{	
-    $.getJSON("http://172.16.153.21:8080/sebo_backendnew/api/productscatalogue/getlist",function (data){
-        $.each(data,function(index,value){
-            var i = index-1;
-           $("#art"+i).append('<h3>' + value.name + '<h3>');
-        });
-        
-        
-    });  
-=======
 function loadFrontArticles()
 {
-    $.getJSON("http://172.16.153.21:8080/sebo_backendnew/api/productscatalogue/getlist", function (data) {
+    $.getJSON("http://172.16.153.31:8080/sebo_backendnew/api/productscatalogue/getlist", function (data) {
 
         $.each(data, function (i, value) {
             var j = i + 1;
             $("#art" + j).append('<h3>' + value.name + '</h3>');
             $("#art" + j).append('<hr>');
-            $("#art" + j).append('<img src="' + value.urlPicture + '"height="42" width="42" />');
+            $("#art" + j).append('<img src="' + value.urlPicture + '"height="50%" width="50%" />');
             $("#art" + j).append('<p>' + value.price + '€</p>');
             $("#art" + j).append('<a href="article.html?id=' + value.id + '"><button class="btn">Voir l\'article</button></a>');
         }
         );
     }
     );
->>>>>>> master
 }
 
 function loadArticle()
@@ -34,16 +21,25 @@ function loadArticle()
     var id = url.substring(url.lastIndexOf("=") + 1);
 
      $.getJSON("http://172.16.153.31:8080/sebo_backendnew/api/productscatalogue/getoneproduct/" + id, function (article) {
-
-<<<<<<< HEAD
-=======
         $("#artTitre").append(article.name);
         $("#artEditeur").append(article.supplier);
         $("#artRef").append(article.reference);
-        $("#artDescription").append('super produit');
-        $("#artPrice").append(article.price + '€');    
+        $("#artDescription").append(article.description);
+        $("#artPrice").append(article.price + " €");
+        $("#artPicture").append('<img src="' + article.urlPicture + '"height="100%" width="100%" />');
+        
     }
     );
 
 }
->>>>>>> master
+
+function refreshPrice()
+{
+    var url = window.location.search;
+    var id = url.substring(url.lastIndexOf("=") + 1);
+     $.getJSON("http://172.16.153.31:8080/sebo_backendnew/api/productscatalogue/getoneproduct/" + id, function (article) {
+       $("#artPrice").text(article.price * $("#selectQty").val() + " €");  
+     });
+    
+
+};
